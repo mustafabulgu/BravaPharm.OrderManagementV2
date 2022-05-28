7863b499eeeab3c3a4d15f96ae65af4134b64cfd
Mustafa Bulgu <mustafabulgu@gmail.com>
2022-05-28 20:23:46 +0100

    Client side logging using serilog.

--- BravaPharm.OrderManagement.App/Program.cs
+++ BravaPharm.OrderManagement.App/Program.cs
@@ -8,29 +8,42 @@ using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.IdentityModel.Logging;
 using MudBlazor.Services;
+using Serilog;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-IdentityModelEventSource.ShowPII = true;
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
-var apiUrl = builder.Configuration["ApiUrl"];
-builder.Services.AddTransient<ApiAuthorizationMessageHandler>();
-builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-
-
-builder.Services.AddHttpClient<IClient, Client>(h => h.BaseAddress = new Uri(apiUrl))
-    .AddHttpMessageHandler<ApiAuthorizationMessageHandler>();
-
-
-
-
-builder.Services.AddScoped<ICategoryDataService, CategoryDataService>();
-builder.Services.AddMudServices();
-builder.Services.AddMsalAuthentication(options =>
+Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+    .WriteTo.BrowserConsole()
+    .CreateLogger();
+try
 {
-    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-    options.ProviderOptions.DefaultAccessTokenScopes.Add(builder.Configuration["AzureAdReadWriteScope"]);
-});
-await builder.Build().RunAsync();
+    IdentityModelEventSource.ShowPII = true;
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
+    Log.Information("Check logging");
+
+    var apiUrl = builder.Configuration["ApiUrl"];
+    builder.Services.AddTransient<ApiAuthorizationMessageHandler>();
+    builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+
+    builder.Services.AddHttpClient<IClient, Client>(h => h.BaseAddress = new Uri(apiUrl))
+        .AddHttpMessageHandler<ApiAuthorizationMessageHandler>();
+
+
+
+
+    builder.Services.AddScoped<ICategoryDataService, CategoryDataService>();
+    builder.Services.AddMudServices();
+    builder.Services.AddMsalAuthentication(options =>
+    {
+        builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+        options.ProviderOptions.DefaultAccessTokenScopes.Add(builder.Configuration["AzureAdReadWriteScope"]);
+    });
+
+    await builder.Build().RunAsync();
+}catch(Exception ex)
+{
+    Log.Error(ex.ToString());
+    throw;
+}
